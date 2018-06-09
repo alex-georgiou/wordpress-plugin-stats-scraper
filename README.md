c1b4eac971a944e288973f36345340c191fb7f65
Alexandros Georgiou <alex.georgiou@gmail.com>
2018-06-09 11:33:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# wordpress-plugin-stats-scraper.js
+
+Scrapes daily installation metrics for a public WordPress plugin hosted on WordPress.org (for use with cron)
+
+## Getting Started
+
+Scrapes and saves the following info:
+
+- `date`: The current date
+- `version`: The most recent plugin version
+- `installsActive`: The current number of installs (rounded to 100's)
+- `installsYesterday`: The number of yesterday's new installs 
+- `installsTotal`: The number of total installs
+- `issuesMonthTotal`: The number of support issues reported in the last month
+- `issuesMonthResolved`: The number of resolved support issues in the last month
+
+
+### Prerequisites
+
+The script is based on PhantomJS. On Linux:
+
+```
+apt-get install phantomjs
+```
+
+### Running
+
+```
+phantomjs wordpress-plugin-stats-scraper.js <plugin_slug> [<output_file.csv>]
+```
+
+* To see usage instructions
+
+```
+phantomjs wordpress-plugin-stats-scraper.js
+```
+
+* To save today's statistics for plugin with slug `akismet`
+
+```
+phantomjs wordpress-plugin-stats-scraper.js akismet
+```
+
+The above will write or append to `akismet-installs.csv` in the current working directory.
+
+* To save today's statistics for plugin with slug `akismet` to file `/home/user/akismet-statistics.csv`:
+
+```
+phantomjs wordpress-plugin-stats-scraper.js akismet /home/user/akismet-statistics.csv
+```
+
+## Built With
+
+* [PhantomJS](http://phantomjs.org/) - Scriptable Headless Browser
+
+## Authors
+
+* **Alexandros Georgiou** - *Initial work* - [github](https://github.com/alex-georgiou) [blog](https://alexgeorgiou.gr)
+
+## License
+
+This project is licensed under the WTFPL License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* A big thank you to the nice people at WordPress.org and to the PhantomJS developers!
---
+++ wordpress-plugin-stats-scraper.js
@@ -0,0 +1,88 @@
+var pjson = require('./package.json');
+var system = require('system');
+var args = system.args;
+
+console.log( pjson.name + ' ' + pjson.version + ' by ' + pjson.author );
+
+if ( args.length === 1 ) {
+	console.log( pjson.description );
+	console.log( "Usage:\n\tphantomjs " + args[0] + ' <plugin_slug> [<output_file.csv>]' );
+	phantom.exit();
+}
+
+var slug = args[1];
+
+var page = require('webpage').create();
+var fs = require('fs');
+
+var url = 'https://wordpress.org/plugins/' + slug + '/advanced/';
+
+var csvFileName;
+if ( args.length > 2 ) {
+	csvFileName = args[2];
+} else {
+	 csvFileName = slug + '-stats.csv';
+}
+
+var headersText = '"date","version","installsActive","installsYesterday","installsTotal","issuesMonthTotal","issuesMonthResolved"';
+
+if ( ! fs.exists( csvFileName ) ) {
+	fs.write( csvFileName, headersText + "\n", 'a' );
+}
+
+console.log( 'Retrieving: ' + url );
+
+page.open( url, function( status ) {
+	if ( 'success' == status ) {
+		var row = {};
+		
+		row.date = new Date().toISOString().split('T')[0];
+
+		row.version = page.evaluate( function() {
+			return jQuery('.widget.plugin-meta li:nth-child(1) strong').text();
+		} );
+		
+		row.installsActive = page.evaluate( function() {
+			return jQuery('.widget.plugin-meta li:nth-child(3) strong').text();
+		} );
+		
+		row.installsYesterday = page.evaluate( function() {
+			return jQuery('#plugin-download-history-stats tbody tr:nth-child(2) td').text();
+		} );
+		
+		row.installsTotal = page.evaluate( function() {
+			return jQuery('#plugin-download-history-stats tbody tr:nth-child(4) td').text();
+		} );
+
+		row.issuesMonthTotal = page.evaluate( function() {
+			return jQuery('.widget.plugin-support .counter-count').text().trim().split(' out of ')[1];
+		} );
+
+		row.issuesMonthResolved = page.evaluate( function() {
+			return jQuery('.widget.plugin-support .counter-count').text().trim().split(' out of ')[0];
+		} );
+
+
+		var rowText =
+			'"' + row.date
+			+ '","' + row.version
+			+ '","' + row.installsActive
+			+ '",' + row.installsYesterday
+			+ ',' + row.installsTotal
+			+ ',' + row.issuesMonthTotal
+			+ ',' + row.issuesMonthResolved;
+
+		console.log( headersText );
+		console.log( rowText );
+
+		console.log( 'Writing to: ' + csvFileName );
+		fs.write( csvFileName, rowText + "\n", 'a' );
+
+	} else {
+		console.err( 'Could not retrieve page' );
+		phantom.exit( 1 );
+	}
+
+	phantom.exit();
+
+} );
